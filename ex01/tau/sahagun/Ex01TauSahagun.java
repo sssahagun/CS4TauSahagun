ed89b34f55a9e4eaeaf3da2284b39c6295365bc0
sssahagun <99473366+sssahagun@users.noreply.github.com>
2022-09-07 08:54:06 +0800

    Add files via upload

---
+++ ex01/tau/sahagun/Ex01TauSahagun.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex01.tau.sahagun;
+
+/**
+ *
+ * @author TAU
+ */
+public class Ex01TauSahagun {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String title1 = "Black Mirror";
+        String title2 = "Criminal Minds";
+        String title3 = "Brooklyn 99";
+        
+        int seasons1 = 5;
+        int seasons2 = 15;
+        int seasons3 = 8;
+        
+        String company1 = "Netflix";
+        String company2 = "CBS";
+        String company3 = "Fox";
+        
+        System.out.printf("Show 1");
+        System.out.printf("Title: " + title1);
+        System.out.printf("Number of seasons: " + seasons1);
+        System.out.printf("First aired by: " + company1);
+    }
+    
+}
