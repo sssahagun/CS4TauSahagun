df9cc12dea9e349087172ce703aae3c84ae7fb29
sssahagun <99473366+sssahagun@users.noreply.github.com>
2022-11-15 08:50:59 +0800

    Add files via upload

--- exercise6/Exercise6.java
+++ exercise6/Exercise6.java
@@ -16,6 +16,13 @@ public class Exercise6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /* create three Monsters, one of each type (you may choose the
+         * values for the Monsters). Simulate 3 separate battles,
+         * Grass vs Fire, Fire vs Water, and Water vs Grass. Create a loop so that they
+         * repeatedly attack each other until one or both HP is reduced to 0. 
+        */
+        
+        SimpleMonster harry = new SimpleMonster("Harry", )
     }
     
 }
